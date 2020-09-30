c1289a9dca7fd25800318b1878df4e5824cea619
Aidan Prior <priorlyaidan@yahoo.com>
2020-09-30 10:59:33 -0400

    done for now, the tables generate randomly and with a good age distribution

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # tables
 A tables generator for camp
 
-The program requires multiple csv files as data input: "Aides.tsv" : A single column row-list of aide names "Camper List.tsv" : A single column row-list of camper names by age (or cabin) "Counselor Tables.tsv" : A row-list of counselors at each table, with a seperate column for each counselor Aide spots are designated with 'Aide'
+The program requires multiple csv files as data input: "Aides.csv" : A single column row-list of aide names "Camper List.csv" : A single column row-list of camper names by age (or cabin) "Counselor Tables.csv" : A row-list of counselors at each table, with a seperate column for each counselor Aide spots are designated with 'Aide'
 
 The program will generate a "Conflicts.csv" file if one does not yet exist, where each column will be a list of campers and counselors with the same last name. These lists may be added to, removed from, or even new lists added to
 
 If the "Conflicts.csv" file already exists, then the program will use those conflicts to generate the tables and save it in a "Tables-Week#.tsv" file and a "Lookup-Week#.tsv"
 
-The program requires two positional commandline arguments, 'start Week' and 'end Week' (both integers) these only affect the resultant filenames. The program will create tables for each week between the two inclusive
+The program requires one commandline argument, the week number to generate for, this only affect the resultant filenames. The program will create tables for the week and then save in the coresponding files.
--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,17 @@
-import Data.List ( foldl, delete, sort, group, sortOn, uncons, transpose )
+{-# OPTIONS_GHC -Wall #-}
+
+import Data.List ( delete, sort, sortOn, uncons, transpose, delete, groupBy )
 import Data.Function ( on )
-import Data.Maybe ( isNothing, fromJust, catMaybes )
+import Data.Maybe ( isNothing, fromJust )
 import System.Environment ( getArgs )
 import qualified Data.Bifunctor as BF ( first )
 import System.IO.Error ( tryIOError )
-import Data.Foldable ( Foldable(toList) ) 
 
 
 import qualified Data.Map.Strict as Map
-import Data.Sequence ( chunksOf, fromList )
 import Data.Text ( splitOn, pack, unpack, strip )
 import System.Random.Shuffle ( shuffle' )
-import System.Random ( RandomGen, split, mkStdGen )
+import System.Random ( RandomGen, split, getStdGen )
 
 (|>) :: a -> (a -> c) -> c
 (|>) = flip ($)
@@ -25,7 +25,7 @@ shuffle'' :: RandomGen gen => [a] -> gen -> [a]
 shuffle'' lst = shuffle' lst (length lst)
 
 tableSize :: Int
-tableSize = 20
+tableSize = 10
 
 data Name = Name {firstN :: String, lastN :: String} deriving (Eq, Ord)
 instance Show Name where
@@ -44,9 +44,9 @@ data Person =
             conflicts :: [Name]}
     deriving (Eq)
 instance Show Person where
-    show (Camper n _ t _) = show n ++ ": " ++ show t  
-    show (Counselor n t _) = show n ++ " at " ++ show t
-    show (Aide n t _) = show n ++ "*: " ++ show t
+    show (Camper n _ tNum _) = show n ++ ": " ++ show tNum  
+    show (Counselor n tNum _) = show n ++ " at " ++ show tNum
+    show (Aide n tNum _) = show n ++ "*: " ++ show tNum
 
 data Table = Table {num :: Int, 
                     counselors :: [Person], 
@@ -56,24 +56,10 @@ data Table = Table {num :: Int,
                     cSlots :: Int} 
                     deriving (Eq)
 instance Show Table where
-    show (Table n cnslrs aideList _ cmprs _ ) = unlines $ ("Table #" ++ show n): concatMap (\(f,s) -> ("\t"++f): map ("\t\t"++) s) [("Counselors:", map (name.>show) cnslrs), ("Aides:", map (name.>show) aideList), ("Campers:", map (name.>show) cmprs)]
+    show (Table n counselorList aideList _ cmprs _ ) = unlines $ ("Table #" ++ show n): concatMap (\(f,s) -> ("\t"++f): map ("\t\t"++) s) [("Counselors:", map (name.>show) counselorList), ("Aides:", map (name.>show) aideList), ("Campers:", map (name.>show) cmprs)]
 instance Ord Table where
     compare = compare `on` num
     (<=) = (<=) `on` num
-
-data SeedResult g a = Success g a | Failure g a 
-
-failed :: SeedResult g a -> Bool
-failed (Success _ _) = False
-failed (Failure _ _) = True
-
-getGen :: SeedResult g a -> g
-getGen (Success g _) = g
-getGen (Failure g _) = g
-
-getResultVal :: SeedResult g a -> a
-getResultVal (Success _ a) = a
-getResultVal (Failure _ a) = a
 
 nameFromString :: String -> Name
 nameFromString s = let s' = words s in Name (head s') (last s')
@@ -84,25 +70,19 @@ allPeople tbl = counselors tbl ++ aides tbl ++ campers tbl
 getAllCampers :: [Table] -> [Person]
 getAllCampers = concatMap campers
 
-getAllCounselors :: [Table] -> [Person]
-getAllCounselors = concatMap counselors
-
 getAllAides :: [Table] -> [Person]
 getAllAides = concatMap aides
 
-getAllPeople :: [Table] -> [Person]
-getAllPeople = concatMap allPeople
-
 generateConflicts :: [Name] -> Person -> Person
 generateConflicts ppl p = 
-    let newConflicts = filter (lastN .> (==) (p |> name |> lastN ) ) ppl
+    let newConflicts = ppl |> filter (lastN .> (==) (p |> name |> lastN )) |> delete (name p)
     in  p {conflicts = newConflicts} 
 
 generateAllConflicts :: [Person] -> Map.Map Name [Name]
-generateAllConflicts ppl = map (generateConflicts (map name ppl)) ppl |> foldl insertToMap Map.empty
+generateAllConflicts ppl = ppl |> map (generateConflicts (map name ppl)) |> filter (conflicts .> null .> not)  |> foldl insertToMap Map.empty
     where
         insertToMap :: Map.Map Name [Name] -> Person -> Map.Map Name [Name]
-        insertToMap mp p = if (name p) `elem` (Map.elems mp |> concat) then mp else Map.insert (name p) (conflicts p) mp
+        insertToMap mp p = if name p `elem` (Map.elems mp |> concat) then mp else Map.insert (name p) (conflicts p) mp
 
 loadConflicts :: Map.Map Name [Name] -> [Person] -> [Person]
 loadConflicts cflcts = map helper
@@ -127,43 +107,50 @@ addAideSlots slotsCounts tbls = zip slotsCounts (sort tbls) |> map updateAideSlo
 addCamperSlots :: [Table] -> [Table]
 addCamperSlots = map (\tbl -> tbl {cSlots = tableSize - (tbl |> counselors |> length) - aSlots tbl})
 
-trySeed :: RandomGen gen => Person -> SeedResult gen [Table] -> SeedResult gen [Table] 
-trySeed a@(Aide _ _ cflcts) sr = 
-    let (g1, g2) = split $ getGen sr
-        tbl:rest = shuffle'' (getResultVal sr) g1 
-    in
-        if filter (flip elem cflcts .> not) (allPeople tbl |> map name) |> length |> (==0)
-        then Success g2 $ tbl{aides= a{t= num tbl} : aides tbl, aSlots= aSlots tbl - 1}:rest 
-        else Failure g2 $ tbl:rest
-    
-trySeed c@(Camper _ _ _ cflcts) sr = 
-    let (g1, g2) = split $ getGen sr
-        tbl:rest = shuffle'' (getResultVal sr) g1 
-    in
-        if filter (flip elem cflcts .> not) (allPeople tbl |> map name) |> length |> (==0)
-        then Success g2 $ tbl{campers= c{t= num tbl} : campers tbl, cSlots= cSlots tbl - 1}:rest 
-        else Failure g2 $ tbl:rest
-
-seed :: RandomGen gen => SeedResult gen [Table] -> Person -> SeedResult gen [Table]
-seed sr p = sr |> iterate (trySeed p) |> tail |> dropWhile failed |> head        
-
 setupTables :: Map.Map Name [Name] -> [Person] -> [Int] -> [Table]
-setupTables cflcts cnslrs aSlotLocs = cnslrs |> loadConflicts cflcts |> createCounselorTables |> addAideSlots aSlotLocs |> addCamperSlots
+setupTables cflcts cs aSlotLocs = cs |> loadConflicts cflcts |> createCounselorTables |> addAideSlots aSlotLocs |> addCamperSlots
+
+trySeed :: Person -> Table -> Maybe Table
+trySeed a@(Aide _ _ cflcts) tbl = 
+    if aSlots tbl > 0 && filter (`elem` cflcts) (allPeople tbl |> map name) |> length |> (==0)
+        then Just tbl{aides= a{t= num tbl} : aides tbl, aSlots= aSlots tbl - 1}
+        else Nothing
+    
+trySeed c@(Camper _ _ _ cflcts) tbl = 
+    if cSlots tbl > 0 && filter (`elem` cflcts) (allPeople tbl |> map name) |> length |> (==0)
+        then Just tbl{campers= c{t= num tbl} : campers tbl, cSlots= cSlots tbl - 1}
+        else Nothing
+
+trySeed (Counselor n _ _) _ = error $ show n ++ " was attempted to be seeded"
+
+seedPerson :: [Table] -> Person -> Maybe [Table]
+seedPerson tbls p = 
+    let f = case p of Camper {} -> cSlots; Aide {} -> aSlots; Counselor n _ _ -> error $ show n ++ " was attempted to be seeded"
+        attempts = tbls |> sortOn (f) |> reverse |> map (trySeed p) |> dropWhile isNothing
+    in
+        if null attempts 
+            then Nothing 
+            else 
+                let tbl = attempts |> head |> fromJust
+                    oldTbl = tbls |> filter (num .> (==) (num tbl) ) |> head
+                in Just $ replace oldTbl tbl tbls
 
 shuffleFoldl :: RandomGen gen => gen -> (b -> a -> b) -> b -> [a] -> b
-shuffleFoldl g f b [] = b
+shuffleFoldl _ _ b [] = b
 shuffleFoldl g f b xs =
     let (g1, g2) = split g
         x:rest = shuffle'' xs g1 
     in shuffleFoldl g2 f (f b x) rest
 
-seedTables :: Int -> [Person] -> [Person] -> [Table] -> [Table]
-seedTables seedNumber aideList cmprs tbls = 
+seedTables :: RandomGen gen => gen -> [Person] -> [Person] -> [Table] -> [Table]
+seedTables seed aideList cmprs tbls = 
     let 
-        gs = mkStdGen seedNumber |> iterate (split .> fst) 
-        cmprGroups = cmprs |> sortOn age |> fromList |> chunksOf (length tbls) |> toList |> map toList |> flip shuffle'' (head gs)
-        sr = shuffleFoldl (gs!!2) seed (Success (gs!!1) tbls) aideList 
-    in shuffleFoldl (gs!!4) (shuffleFoldl (gs!!3) seed) sr cmprGroups |> getResultVal
+        (g1, g2) = seed |> split
+        start = shuffleFoldl g2 (fromJust .> seedPerson) (Just tbls) aideList
+        cmprGroups = cmprs |> sortOn age |> groupBy ((==) `on` age) |> flip shuffle'' g1
+    in
+        foldl (shuffleFoldl g1 (fromJust .> seedPerson)) start cmprGroups |> fromJust
+       
 
 importAides :: FilePath -> IO [Person]
 importAides fp = do
@@ -178,27 +165,27 @@ importCounselorTables fp = do
     raw |> lines |> zip [1..] |> map importCounselorLine |> unzip |> BF.first concat |> return
     where
         importCounselorLine :: (Int, String) -> ([Person], Int)
-        importCounselorLine (t, l) = 
+        importCounselorLine (tNo, l) = 
             let 
                 fields = l |> pack |> splitOn (pack ",") |> map (strip .> unpack)
-                cnslrs = fields |> filter (/= "Aide") |> map (\n -> Counselor (nameFromString n) t [])
+                allCounselors = fields |> filter (/= "Aide") |> map (\n -> Counselor (nameFromString n) tNo [])
                 aslots = fields |> filter (== "Aide") |> length
             in 
-                (cnslrs, aslots)
+                (allCounselors, aslots)
 
-importCampers :: FilePath -> IO [Person]
-importCampers fp = do
+importCampers :: FilePath -> Int -> IO [Person]
+importCampers fp numTables = do
     raw <- readFile fp
-    raw |> lines |> zip [1..] |> map importCamperLine |> return
+    raw |> lines |> zip [0..] |> map importCamperLine |> return
     where
         importCamperLine :: (Int, String) -> Person
-        importCamperLine (a, l) = l |> takeWhile (/= ',') |> nameFromString |> flip (flip (`Camper` a) 0) []
+        importCamperLine (a, l) = l |> takeWhile (/= ',') |> nameFromString |> flip (flip (`Camper` (a `div` numTables) ) 0) []
 
 importConflicts :: FilePath -> IO (Maybe (Map.Map Name [Name]))
 importConflicts fp = do
     raw <- tryIOError $ readFile fp
     case raw of
-        Left e -> return Nothing
+        Left _ -> return Nothing
         Right str -> 
             let
                 importConflictLine :: String -> (Name, [Name])
@@ -208,33 +195,34 @@ importConflicts fp = do
     
 
 exportConflicts :: FilePath -> Map.Map Name [Name] -> IO ()
-exportConflicts fp cflcts = do cflcts |> Map.toList |> map (\(f,s) -> f:s) |> map (concatMap (show .> (++) ",")) |> unlines |> writeFile fp 
+exportConflicts fp cflcts = do cflcts |> Map.toList |> map (uncurry (:)) |> map (concatMap (show .> (++ ",") ) .> init) |> unlines |> writeFile fp 
 
 createLookup :: [Table] -> [String]
-createLookup tbls = pplLookup (getAllCampers tbls) |> (++) ("" : pplLookup (getAllAides tbls))
+createLookup tbls = pplLookup (getAllCampers tbls) |> (++ ("" : pplLookup (getAllAides tbls)))
     where
         pplLookup = sortOn (name .> lastN) .> map (\c -> lastN (name c) ++ "," ++ firstN (name c) ++ "," ++ show (t c)) 
 
 createTables :: [Table] -> [String]
-createTables tbls = headers ++ names
+createTables tbls = headers : names
     where
-        headers = tbls |> map (num .> show) |> map ("Table " ++) |> map (++ ",")
-        names = tbls |> map (campers .> map name) |> transpose |> map (show .> (++) ",")
+        maxLength = tbls |> map (campers .> length) |> maximum
+        emptyFill tbl = tbl{ campers =tbl |> campers |> (++ replicate (tbl |> campers |> length |> (maxLength-)) (Camper (Name "" "") 1000 0 []))}
+
+        tbls' = tbls |> sort |> map (\tbl -> tbl {campers= campers tbl |> sortOn age}) |> map emptyFill
+        headers = tbls' |> map (num .> show) |> map ("Table " ++) |> concatMap (++ ",") |> init
+        names = tbls' |> map (campers .> map name) |> transpose |> map (concatMap (show .> (++ ",")) .> init)
 
 exportTablesAndLookup :: FilePath -> [Table] -> IO ()
 exportTablesAndLookup fp tbls = do
     tbls |> createLookup |> unlines |> writeFile ("Lookup-" ++ fp) 
     tbls |> createTables |> unlines |> writeFile ("Tables-" ++ fp)
 
-
-main :: IO ()
-main = do
-    weekNumber <- getArgs
-    seed <- getLine
+run :: RandomGen gen => Int -> gen -> IO ()
+run weekNumber seedNumber = do
     mbCflcts <- importConflicts "Conflicts.csv"
     (simple_counselors, aSlotLocs) <- importCounselorTables "Counselor Tables.csv"
     simple_aides <- importAides "Aides.csv"
-    simple_campers <- importCampers "Campers.csv"
+    simple_campers <- importCampers  "Camper List.csv" (length aSlotLocs)
     if isNothing mbCflcts
         then (simple_counselors ++ simple_aides ++ simple_campers) |> generateAllConflicts |> exportConflicts "Conflicts.csv" 
         else 
@@ -242,7 +230,10 @@ main = do
             ads = loadConflicts cflcts simple_aides
             cmprs = loadConflicts cflcts simple_campers
             in
-            setupTables cflcts simple_counselors aSlotLocs |> seedTables (read seed) ads cmprs |> exportTablesAndLookup ("Week" ++ show (head weekNumber) ++ ".csv")
+            setupTables cflcts simple_counselors aSlotLocs |> seedTables seedNumber ads cmprs |> exportTablesAndLookup ("Week" ++ show weekNumber ++ ".csv")
 
-cnslrs :: [Person]
-cnslrs = zip ['a'..'z'] [1..] |> map (\(n,t) -> Counselor (Name "Counselor" (n:"")) t [])
+main :: IO ()
+main = do
+    weekNumber <- getArgs
+    seedNumber <- getStdGen
+    run (read $ head weekNumber) seedNumber
